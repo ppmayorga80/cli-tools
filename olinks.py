36c0b88981536070eb6ae704c81df495b491e69c
Pedro Mayorga <ppmayorga80@gmail.com>
2024-11-18 23:43:02 -0600

    dedup urls

--- olinks.py
+++ olinks.py
@@ -8,6 +8,13 @@ def clean_string(text):
     text = re.sub(r"['\"]+","", text)
     return text
 
+def dedup(data):
+    new_data = []
+    for x in data:
+        if x not in new_data:
+            new_data.append(x)
+    return new_data
+
 def open_links():
     content = pyperclip.paste()
     if content is None:
@@ -15,7 +22,8 @@ def open_links():
 
     lines = [clean_string(x) for x in str(content).split("\n")]
     urls = [line for line in lines if line.startswith("http://") or line.startswith("https://")]
-
+    urls = dedup(urls)
+    
     for url in urls:
         os.system(f"open {url}")
 
