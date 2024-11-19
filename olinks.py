1b880f533b3d250cbf64126e5d7fa43f6a6367a1
Pedro Mayorga <ppmayorga80@gmail.com>
2024-11-19 00:04:38 -0600

    update the way to recognize urls

--- olinks.py
+++ olinks.py
@@ -3,10 +3,22 @@ import re
 
 import pyperclip
 
+
 def clean_string(text):
     text = text.strip()
-    text = re.sub(r"['\"]+","", text)
+    text = re.sub(r"['\"]+", "", text)
     return text
+
+
+def text_to_valid_urls(content):
+    lines = [f"http{x}" for x in content.split("http")]
+    lines = [xk.strip() for x in lines for xk in x.split("\n")]
+    lines = [clean_string(x) for x in lines]
+
+    regex = re.compile(r"^https?:.*$")
+    lines = [x for x in lines if x and regex.findall(x)]
+    return lines
+
 
 def dedup(data):
     new_data = []
@@ -15,15 +27,15 @@ def dedup(data):
             new_data.append(x)
     return new_data
 
+
 def open_links():
     content = pyperclip.paste()
     if content is None:
         return
 
-    lines = [clean_string(x) for x in str(content).split("\n")]
-    urls = [line for line in lines if line.startswith("http://") or line.startswith("https://")]
+    urls = text_to_valid_urls(str(content))
     urls = dedup(urls)
-    
+
     for url in urls:
         os.system(f"open {url}")
 
